1aaadce810cc0fc1f2e27bce15dcd8a5015ecd59
Andy Strube <andystrube64@gmail.com>
2023-01-25 13:10:04 -0800

    Fixed input element bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,15 @@ class CommentsController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @chatroom.comments.create(comment_params)
-    Turbo::StreamsChannel.broadcast_update_to(
-      "chatroom_channel",
-      partial: "chatrooms/comments",
-      target: "target",
-      locals: {comments: @chatroom.comments}
-    )
+    respond_to do |format|
+      format.html { redirect_to chatroom_path(@chatroom) }
+      Turbo::StreamsChannel.broadcast_update_to(
+        "chatroom_channel",
+        partial: "chatrooms/comments",
+        target: "target",
+        locals: {comments: @chatroom.comments}
+      )
+    end
   end
 
   private
